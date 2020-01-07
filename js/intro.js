b398c83fb4d49fee465d9b95eda662bbe66f6293
Jalolov S <jsultonali1993@mail.ru>
2020-01-07 19:21:33 +0500

    Структурировано до game-1.js

--- js/intro.js
+++ js/intro.js
@@ -1,19 +1,20 @@
 
 import {changeScreen, render} from "./util";
-import moduleGreetingElement from "./greeting";
+import moduleGreetingFunc from "./greeting";
+import {greeting} from "./data";
 
-const template = `<section class="intro">
+export default (data) => {
+  const template = `<section class="intro">
     <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
-    <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+    <p class="intro__motto"><sup>*</sup>${data[`description`]}</p>
   </section>`;
 
-const moduleIntroElement = render(template);
+  const moduleIntroElement = render(template);
 
-const agreeButton = moduleIntroElement.querySelector(`.intro__asterisk`);
+  const agreeButton = moduleIntroElement.querySelector(`.intro__asterisk`);
 
-agreeButton.addEventListener(`click`, () => {
-  changeScreen(moduleGreetingElement);
-});
-
-
-export default moduleIntroElement;
+  agreeButton.addEventListener(`click`, () => {
+    changeScreen(moduleGreetingFunc(greeting));
+  });
+  return moduleIntroElement;
+};
--- js/rules.js
+++ js/rules.js
@@ -1,58 +1,48 @@
 
 import {changeScreen, render} from "./util";
-import moduleGame1Element from "./game-1";
-import moduleGreetingElement from "./greeting";
+import moduleGame1Func from "./game-1";
+import {game1} from "./data";
+import headerTemplateFunc from "./header";
 
-const template = `<header class="header">
-    <button class="back">
-      <span class="visually-hidden">Вернуться к началу</span>
-      <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
-        <use xlink:href="build/img/sprite.svg#arrow-left"></use>
-      </svg>
-      <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
-        <use xlink:href="build/img/sprite.svg#logo-small"></use>
-      </svg>
-    </button>
-  </header>
-  <section class="rules">
-    <h2 class="rules__title">Правила</h2>
+
+export default (data) => {
+  const template = `<section class="rules">
+    <h2 class="rules__title">${data.title}</h2>
     <ul class="rules__description">
-      <li>Угадай 10 раз для каждого изображения фото
+      <li>${data.descriptionList[0]}
         <img class="rules__icon" src="img/icon-photo.png" width="32" height="31" alt="Фото"> или рисунок
         <img class="rules__icon" src="img/icon-paint.png" width="32" height="31" alt="Рисунок"></li>
-      <li>Фотографиями или рисунками могут быть оба изображения.</li>
-      <li>На каждую попытку отводится 30 секунд.</li>
-      <li>Ошибиться можно не более 3 раз.</li>
+      <li>${data.descriptionList[1]}</li>
+      <li>${data.descriptionList[2]}</li>
+      <li>${data.descriptionList[3]}</li>
     </ul>
-    <p class="rules__ready">Готовы?</p>
+    <p class="rules__ready">${data.ready}</p>
     <form class="rules__form">
       <input class="rules__input" type="text" placeholder="Ваше Имя">
       <button class="rules__button  continue" type="submit" disabled>Go!</button>
     </form>
   </section>`;
 
-const moduleRulesElement = render(template);
 
-const rulesButton = moduleRulesElement.querySelector(`.rules__button`);
-rulesButton.addEventListener(`click`, () => {
-  rulesInput.value = ``;
-  changeScreen(moduleGame1Element);
-});
 
-const rulesInput = moduleRulesElement.querySelector(`.rules__input`);
-rulesInput.addEventListener(`input`, () => {
-  if (rulesInput.value.length >= 2) {
-    rulesButton.removeAttribute(`disabled`);
-  }
-  if (rulesInput.value.length < 2) {
-    rulesButton.setAttribute(`disabled`, `true`);
-  }
-});
+  const moduleRulesElement = render(template);
 
-const back = moduleRulesElement.querySelector(`.back`);
-back.addEventListener(`click`, () => {
-  rulesInput.value = ``;
-  changeScreen(moduleGreetingElement);
-});
+  const rulesButton = moduleRulesElement.querySelector(`.rules__button`);
+  rulesButton.addEventListener(`click`, () => {
+    rulesInput.value = ``;
+    changeScreen(moduleGame1Func(game1));
+  });
 
-export default moduleRulesElement;
+  const rulesInput = moduleRulesElement.querySelector(`.rules__input`);
+  rulesInput.addEventListener(`input`, () => {
+    if (rulesInput.value.length >= 2) {
+      rulesButton.removeAttribute(`disabled`);
+    }
+    if (rulesInput.value.length < 2) {
+      rulesButton.setAttribute(`disabled`, `true`);
+    }
+  });
+  moduleRulesElement.prepend(headerTemplateFunc());
+
+  return moduleRulesElement;
+};
